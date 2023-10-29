ad837cc42224f3a9bedfc1747c1f6455f9b881e1
Dumi <george.dumitrache99@yahoo.com>
2023-10-29 14:43:57 +0200

    Clean code
    Clean code

--- src/App.js
+++ src/App.js
@@ -18,47 +18,7 @@ function App() {
   return (
     <>
       <RouterProvider router={router} />
-      <div className="d-flex flex-column p-5">
-        <p className="heading-xl">Test</p>
-        <p className="heading-l">Test</p>
-        <p className="heading-m">Test</p>
-        <p className="heading-s">Test</p>
-        <p className="body-1">Test</p>
-        <p className="body-2">Test</p>
-        <button className="btn-dine my-3">Test</button>
-        <button className="btn-dine my-3 inactive">Test</button>
-        <div className="bg-dark d-flex flex-column p-5 my-5">
-          <button className="btn-dine-dark my-3">Test</button>
-          <button className="btn-dine-dark my-3 inactive">Test</button>
-        </div>
-        <input type="text" placeholder="test" value={"Test"} />
-        <input
-          type="text"
-          className="my-5"
-          placeholder="test"
-        />
-        <div className="d-flex flex-column my-5">
-          <input
-            type="text"
-            className="error"
-            placeholder="test"
-            value={"Test"}
-          />
-          <span className="input-error">This field is required</span>
-        </div>
-        <select className="my-5">
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-        </select>
-        <select disabled className="my-5">
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-        </select>
-        <input type="number" className="my-5" placeholder="test" value={0} />
-        <input type="number" className="my-5" disabled placeholder="test" value={0} />
-      </div>
+      
     </>
   );
 }
