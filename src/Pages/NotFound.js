ec08d83125d38b27335fa86c936ae570876ce23f
Dumi <george.dumitrache99@yahoo.com>
2023-10-29 15:04:01 +0200

    Not found page
    Not found page

---
+++ src/Pages/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="not-found-page d-flex flex-column justify-content-center align-items-center">
+            <h1 className="heading-xl">404: Page Not Found</h1>
+            <a className="link-1 body-2 mt-4" href='/'>Return to homepage</a>
+        </div>
+    )
+}
+
+export default NotFound;
