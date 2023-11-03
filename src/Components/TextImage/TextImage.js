12c10bbb3687156bf8aaa0fedbe336ff5a6d5611
Szidelo <claudiuszidelo@yahoo.ro>
2023-11-03 19:46:42 +0200

    text-image in progress

--- src/Components/TextImage/TextImage.js
+++ src/Components/TextImage/TextImage.js
@@ -1,9 +1,25 @@
-import './TextImage.css';
+import "./TextImage.css";
+import img from '../../assets/images/img_2.png'
+import lines from '../../assets/images/lines.svg'
+import headingSvg from '../../assets/images/circle_and_line.svg'
 
-const TextImage = () => {
-    return (
-        <></>
-    )
-}
+const TextImage = ({data}) => {
+	return (
+		<div className="row img-text ">
+			<div className="col-6 d-flex justify-content-center align-items-start flex-column gap-4">
+                <img className="mb-5" src={headingSvg} alt=""/>
+				<p className="heading-l">{data.title}</p>
+				<p className="body-1">{data.desc}</p>
+			</div>
+			<div className="col-6 d-flex justify-content-end position-relative">
+                <img className="text-img-svg" src={lines} alt=""/>
+				<img
+					src={img}
+					alt=""
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default TextImage;
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,12 +1,14 @@
 import data from '../utils/data.json'
 import BannerHP from "../Components/BannerHP/BannerHP";
 import ImageText from "../Components/ImageText/ImageText";
+import TextImage from '../Components/TextImage/TextImage';
 
 const Homepage = () => {
   return (
     <>
       <BannerHP data={data}/>
       <ImageText />
+      <TextImage data={data.text_image}/>
     </>
   );
 };
